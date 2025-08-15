2251e2e6104e3749e02eea3b76c12e5747f908e7
Ashir Ali <ashirfabtechsol@gmail.com>
2025-08-16 02:20:44 +0400

    Add CSRF_TRUSTED_ORIGINS

--- OpticorAI_project_management_system/settings/prod.py
+++ OpticorAI_project_management_system/settings/prod.py
@@ -6,7 +6,15 @@ DEBUG = False
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'web-production-60bd4.up.railway.app').split(',') if os.environ.get('DJANGO_ALLOWED_HOSTS') else []
+ALLOWED_HOSTS = [
+    'web-production-60bd4.up.railway.app',
+    '127.0.0.1',
+    'localhost'
+] if os.environ.get('DJANGO_ALLOWED_HOSTS') else []
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-60bd4.up.railway.app',
+]
 
 # Security hardening
 SECURE_HSTS_SECONDS = int(os.environ.get('DJANGO_SECURE_HSTS_SECONDS', '31536000'))
