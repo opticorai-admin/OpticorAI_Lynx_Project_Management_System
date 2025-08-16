6b9a657f0612d2a358d7186105ef50936fbad780
Ashir Ali <ashirfabtechsol@gmail.com>
2025-08-16 22:24:55 +0400

    Settings Update

--- OpticorAI_project_management_system/settings.py
+++ OpticorAI_project_management_system/settings.py
@@ -17,10 +17,15 @@ ALLOWED_HOSTS = os.environ.get(
     'web-production-60bd4.up.railway.app,127.0.0.1,localhost'
 ).split(',')
 
-CSRF_TRUSTED_ORIGINS = os.environ.get(
-    'DJANGO_CSRF_TRUSTED_ORIGINS',
+CSRF_TRUSTED_ORIGINS = [
     'https://web-production-60bd4.up.railway.app'
-).split(',')
+]
+
+
+# CSRF_TRUSTED_ORIGINS = os.environ.get(
+#     'DJANGO_CSRF_TRUSTED_ORIGINS',
+#     'https://web-production-60bd4.up.railway.app'
+# ).split(',')
 
 # APPS
 INSTALLED_APPS = [
