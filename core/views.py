e5d01ed136e20766d8e62515b6fecefdcd76f624
Ashir Ali <ashirfabtechsol@gmail.com>
2025-08-17 17:24:46 +0400

    Fix Image Display Issues and update html second time

--- core/views.py
+++ core/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from datetime import date
 import os
+from django.conf import settings
 from django import forms
 from django.http import JsonResponse
 from django.views import View
